db02b267bc1d39ae738e27e9ce54797eeff3e865
Aki-dotcom <179094797+Aki-dotcom@users.noreply.github.com>
2025-09-09 14:18:10 -0300

    Add files via upload

---
+++ Abstract Factory/ConsoleApp1/GoogleMaps/GoogleMapa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GoogleMapa:IMapa
+    {
+        public void Dibujar(string ubicacion) => Console.WriteLine("GoogleMaps mostrando: Argentina, Mar del Plata");
+    }
+}
