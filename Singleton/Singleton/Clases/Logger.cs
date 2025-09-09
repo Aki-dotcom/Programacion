dfe14d628745ece9c42a8067afbda78bedcbfc2b
Aki-dotcom <179094797+Aki-dotcom@users.noreply.github.com>
2025-09-09 16:11:21 -0300

    Add files via upload

---
+++ Singleton/Singleton/Clases/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Clases
+{
+    public sealed class Logger
+    {
+        private static readonly Lazy<Logger> _instancia = new(() => new Logger());
+        public static Logger Instancia => _instancia.Value;
+
+        private readonly List<string> _historial = new();
+
+        private Logger() { }
+
+        public void Info(string mensaje)
+        {
+            var log = $"[INFO] {DateTime.Now:HH:mm:ss} - {mensaje}";
+            _historial.Add(log);
+            Console.WriteLine(log);
+        }
+
+        public void Dump()
+        {
+            Console.WriteLine("=== HISTORIAL DE LOGS ===");
+            foreach (var msg in _historial)
+                Console.WriteLine(msg);
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using Singleton.Clases;
+using System;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Logger.Instancia;
+            a.Info("Aplicación iniciada");
+            a.Info("Conectando a la base de datos");
+
+           
+            var b = Logger.Instancia;
+            b.Info("Proceso completado correctamente");
+
+            
+            b.Dump();
+
+            
+            Console.WriteLine($"¿Misma instancia? {ReferenceEquals(a, b)}");
+        }
+    }
+}
